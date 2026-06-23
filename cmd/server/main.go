af962a85113f6954b504f33ff50151632bbdd736
mmulenga <matt.mulengawoo@gmail.com>
2026-06-23 12:17:40 +0000

    Implemented connection pooling and goose migration on startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"context"
+	"embed"
 	"fmt"
-    "database/sql"
-    "embed"
-    "github.com/pressly/goose/v3"
+	"os"
+
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/pressly/goose/v3"
 )
 
-    //go.embed migrations/*.sql
+//go.embed migrations/*.sql
     var embedMigrations embed.FS
 
 func main() {
-    var db *sql.DB
-    goose.SetBaseFS(embedMigrations)
+    ctx := context.Background()
 
+    // Create a connection pool to the db
+    pool, err := pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
+    if err != nil {
+        fmt.Print("Unable to create connection pool: ", err)
+    }
+    if err := pool.Ping(ctx); err != nil { 
+        fmt.Println("Unable to reach the database: ", err)
+    }
+    defer pool.Close()
+
+    db := stdlib.OpenDBFromPool(pool)
+    defer db.Close()
+
+    
+    goose.SetBaseFS(embedMigrations)
     if err := goose.SetDialect("postgres"); err != nil {
         panic(err)
     }
 
+    // Perform db migrations using goose
     if err := goose.Up(db, "migrations"); err != nil {
         panic(err)
     }
